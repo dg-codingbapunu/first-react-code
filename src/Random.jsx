41393294e67b45ff7ff81d489d79d045f7dc853a
patitapaban <patitapabanpanda046@gmail.com>
2024-04-09 07:41:30 +0530

    export/import

--- src/Random.jsx
+++ src/Random.jsx
@@ -1,10 +1,6 @@
 function Random() {
   let Number = Math.random() * 100;
 
-  return (
-    <h1 style={{ "background-color": "#776912" }}>
-      random number is {Math.round(Number)}
-    </h1>
-  );
+  return <h1>random number is {Math.round(Number)}</h1>;
 }
 export default Random;
