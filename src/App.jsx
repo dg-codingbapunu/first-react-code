b86df27db73b09b2b3db716c52c2e1fa4fc62b23
patitapaban <patitapabanpanda046@gmail.com>
2024-04-09 07:36:43 +0530

    reusable

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Learn from "./Learn";
+import Random from "./Random";
 function App() {
   return (
     <div>
       <h1>this is best course on react</h1>
       <h3>hyy im yes why this is </h3>
       <button>click me</button>
-      <Learn></Learn>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
     </div>
   );
 }
