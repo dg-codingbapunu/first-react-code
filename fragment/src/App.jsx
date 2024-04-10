f8bfb8ec4562dbc47a478455932f7a580e756f91
patitapaban <patitapabanpanda046@gmail.com>
2024-04-10 09:47:05 +0530

    first commit

--- fragment/src/App.jsx
+++ fragment/src/App.jsx
@@ -2,15 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  let foodItem = ["Dal", "Sabji", "Milk", "Roti", "Ghee", "Curd", "ghee"];
-  // let foodItem = [];
-  if (foodItem.length === 0) {
-    return <h4>Im hungry</h4>;
-  }
+  // let foodItem = ["Dal", "Sabji", "Milk", "Roti", "Ghee", "Curd", "ghee"];
+  let foodItem = [];
 
   return (
     <>
       <h1>Healthy Food</h1>
+      {foodItem.length === 0 ? <h4>Im hungry</h4> : null}
       <ul className="list-group">
         {foodItem.map((item) => (
           <li key={item} className="list-group-item">
