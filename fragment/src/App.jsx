cfd1f68f878ee955ec564bb04868d3a8ec62ccd4
patitapaban <patitapabanpanda046@gmail.com>
2024-04-10 09:50:18 +0530

    ternary operater in rea t

--- fragment/src/App.jsx
+++ fragment/src/App.jsx
@@ -4,11 +4,12 @@ import "./App.css";
 function App() {
   // let foodItem = ["Dal", "Sabji", "Milk", "Roti", "Ghee", "Curd", "ghee"];
   let foodItem = [];
+  let emptyItem = foodItem.length === 0 ? <h4>Im hungry</h4> : null;
 
   return (
     <>
       <h1>Healthy Food</h1>
-      {foodItem.length === 0 ? <h4>Im hungry</h4> : null}
+      {emptyItem}
       <ul className="list-group">
         {foodItem.map((item) => (
           <li key={item} className="list-group-item">
