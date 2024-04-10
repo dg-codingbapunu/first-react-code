5142bdd1ac6649fe5d006442faf285fe2ac6668a
patitapaban <patitapabanpanda046@gmail.com>
2024-04-10 09:37:27 +0530

    if condition in react

--- fragment/src/App.jsx
+++ fragment/src/App.jsx
@@ -2,12 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  let FoodItem = ["Dal", "Sabji", "Milk", "Roti", "Ghee", "Curd", "ghee"];
+  let foodItem = ["Dal", "Sabji", "Milk", "Roti", "Ghee", "Curd", "ghee"];
+  // let foodItem = [];
+  if (foodItem.length === 0) {
+    return <h4>Im hungry</h4>;
+  }
+
   return (
     <>
       <h1>Healthy Food</h1>
       <ul className="list-group">
-        {FoodItem.map((item) => (
+        {foodItem.map((item) => (
           <li key={item} className="list-group-item">
             {item}
           </li>
