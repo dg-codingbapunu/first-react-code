dde5ef5ab526a63d326d41222989802d90c214ec
patitapaban <patitapabanpanda046@gmail.com>
2024-04-10 06:58:41 +0530

    fragment method in react

---
+++ fragment/src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">sabji</li>
+        <li class="list-group-item">milk</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">Curd</li>
+      </ul>
+    </>
+  );
+}
+export default App;
