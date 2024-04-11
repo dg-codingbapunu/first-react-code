18bdd8b4ca2abb658be723fe737e298cbc8c84d2
patitapaban <patitapabanpanda046@gmail.com>
2024-04-11 21:25:46 +0530

    head section

--- project/calculator/src/App.jsx
+++ project/calculator/src/App.jsx
@@ -1,9 +1,4 @@
 function App() {
-  return <h6>im here</h6>;
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat unde
-    repellendus soluta officia in quos consectetur nihil cumque veniam est vel
-    quae quasi labore a fuga accusantium, at obcaecati! Rem.
-  </p>;
+  return <h1>CALCULATOR</h1>;
 }
 export default App;
