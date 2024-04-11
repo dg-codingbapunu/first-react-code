96137c08ead775eeb65c5e006ee7b94bbc8659c0
patitapaban <patitapabanpanda046@gmail.com>
2024-04-11 21:24:04 +0530

    commit

--- project/calculator/src/App.jsx
+++ project/calculator/src/App.jsx
@@ -1,4 +1,9 @@
 function App() {
   return <h6>im here</h6>;
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat unde
+    repellendus soluta officia in quos consectetur nihil cumque veniam est vel
+    quae quasi labore a fuga accusantium, at obcaecati! Rem.
+  </p>;
 }
 export default App;
