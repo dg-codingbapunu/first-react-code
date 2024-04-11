245f5513e0fe502c50a9516e4b5e549fac391bd0
patitapaban <patitapabanpanda046@gmail.com>
2024-04-11 09:14:09 +0530

    props method

--- fragment/src/Item.jsx
+++ fragment/src/Item.jsx
@@ -1,4 +1,4 @@
-function Item(props) {
-  return <li className="list-group-item">{props.foodItem}</li>;
+function Item(items) {
+  return <li className="list-group-item">{items.foodItem}</li>;
 }
 export default Item;
