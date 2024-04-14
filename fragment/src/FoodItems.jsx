8678806d4536292dab726888d50b5f4911377542
patitapaban <patitapabanpanda046@gmail.com>
2024-04-14 12:26:22 +0530

    add bootstrap active class

--- fragment/src/FoodItems.jsx
+++ fragment/src/FoodItems.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import Item from "./Item";
 function FoodItems({ items }) {
+  let [activeItems, setActiveItems] = useState([]);
+  let onBuybutton = (item, event) => {
+    let newItems = [...activeItems, item];
+    setActiveItems(newItems);
+  };
   return (
     <ul className="list-group">
       {items.map((item) => (
         <Item
           key={item}
           foodItem={item}
-          handleBuyButton={() => console.log(`${item} being bought`)}
+          bought={activeItems.includes(item)}
+          handleBuyButton={(event) => onBuybutton(item, event)}
         ></Item>
       ))}
     </ul>
