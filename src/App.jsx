70fefe1cf871e3ec99f2bbb44627a570c8393732
patitapaban <patitapabanpanda046@gmail.com>
2024-04-15 12:59:47 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import AppName from "./component/AppName";
 
 import TodoItems from "./2 create react components/TodoItems";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const todoItems = [
+  const inttodoItems = [
     {
       name: "Go To College",
       dueDate: "20/04/2024",
@@ -19,6 +20,8 @@ function App() {
       dueDate: "14/02/2024",
     },
   ];
+  const [todoItems, setTodoitems] = useState(inttodoItems);
+
   return (
     <center className="todo-container">
       <AppName></AppName>
